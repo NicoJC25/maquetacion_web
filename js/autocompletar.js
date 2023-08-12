88ca4f5c512f545c3ff9bd1123ebe1cb192670d4
NicoJC25 <nejuez@misena.edu.co>
2023-08-11 21:09:42 -0500

    Finalizacion de la pagina web con la logica dejs y los estilos ajsutados, ademas de algunas pruebas reealizadas con otros formatos y un archivo tipo manual explicando el contenido del proyecto

--- js/autocompletar.js
+++ js/autocompletar.js
@@ -6,36 +6,62 @@ let palabrasclave = [
     'Politicos(Premium)'
 ];
 
-const cajaResultado = document.querySelector(".busqueda");
-const cajaEntrada = document.getElementById("buscar");
+const cajaBuscador = document.querySelector(".buscador");
+const cajaEntrada = cajaBuscador.querySelector("input");
+const cajaBusqueda = cajaBuscador.querySelector(".busqueda");
+const icon = cajaBuscador.querySelector(".icon")
+let marcadorLink = cajaBuscador.querySelector("a");
+let linkPagina;
 
-cajaEntrada.onkeyup = function(){
-    let resultado = [];
-    let entrada = cajaEntrada.value;
-    if (entrada.length){
-        resultado = palabrasclave.filter((keyword) => {
-            return keyword.toLowerCase().includes(entrada.toLowerCase());
-        });
-        console.log(resultado)
-    }
-    display(resultado);
 
-    if (!resultado.length){
-        cajaResultado.innerHTML = '';
-    }
-}
+cajaEntrada.onkeyup = (e) => {
+    let resultados = [];
+    let datoUsuario = e.target.value;
+    if (datoUsuario){
+        
+        icon.onclick = () => {
+            linkPagina = `https://www.google.com/search?q=${datoUsuario}`;
+            marcadorLink.setAttribute("href", linkPagina);
+            marcadorLink.click();
+        }
 
-function display(resultado){
-    const contenido = resultado.map((list) => {
-        return '<li onclick=selectInput(this)>' + list +"</li>";
+        resultados = palabrasclave.filter((data) => {
+            return data.toLocaleLowerCase().startsWith(datoUsuario.toLocaleLowerCase());
+
+    });
+        resultados = resultados.map((data) => {
+            return data = `<li>${data}</li>`
     });
 
-    cajaResultado.innerHTML = '<ul>' + contenido.join('') + '</ul>';
+    cajaBuscador.classList.add("active");
+    mostrarResultados(resultados);
+    let todaLista = cajaBusqueda.querySelectorAll("li");
+    for(let i = 0; i< todaLista.length; i++) {
+        todaLista[i].setAttribute("onclick", "seleccionar(this)");
+    }
+    }else{
+        cajaBuscador.classList.remove("active")
+    } 
 }
 
-function selectInput(list){
-    cajaEntrada.value = list.innerHTML;
-    cajaResultado.innerHTML = '';
+function seleccionar(element){
+    let seleccionarDato = element.textContent;
+    cajaEntrada.value = seleccionarDato;
+    icon.onclick = () => {
+        linkPagina = `https://www.google.com/search?q=${seleccionarDato}`;
+        marcadorLink.setAttribute("href", linkPagina);
+        marcadorLink.click();
+    }
+    cajaBuscador.classList.remove("active")
 }
-
-
+        
+function mostrarResultados(list){
+    let listaDatos;
+    if(!list.length){
+        valorUsuario = cajaEntrada.value;
+        listaDatos = `<li>${valorUsuario}</li>`
+    }else{
+        listaDatos = list.join('');
+    }
+    cajaBusqueda.innerHTML = listaDatos;
+}
---
+++ js/credenciales.js
@@ -0,0 +1,91 @@
+//Ejecuciones para que el sistema de estilos del formulario funcione
+
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn__registrarse").addEventListener("click", register);
+window.addEventListener("resize", anchoPagina); //Diseño responsivo cuando se ajuste el tamaño de la pagina
+
+//Declaracion de variables para el estilo del formulario
+
+var contenedor_login_register = document.querySelector(".contenedor__login-register");
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var caja_trasera_login = document.querySelector(".caja__trasera-login");
+var caja_trasera_register = document.querySelector(".caja__trasera-register");
+
+//Funcion para que se ajuste el ancho de la pagina
+
+function anchoPagina(){
+    if (window.innerWidth > 850){
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px"
+    }
+}
+
+//Se ejecuta para que tambien funcione cuando se recargue la pagina
+
+anchoPagina();
+
+//Funcion para estilos cuando se quiera iniciar sesion y ajustar la pagina
+
+function iniciarSesion(){
+
+    if(window.innerWidth > 850){
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    }else{
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "none";
+    }
+}
+
+//Funcion para estilos cuando se quiera registrar y ajustar la pagina
+
+function register(){
+    if(window.innerWidth > 850){
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    }else{
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+    }
+}
+
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.formulario__login')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    }, false)
+    })
+})()
